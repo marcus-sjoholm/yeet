9c622df43c7fe21d9d90636772646ac5000b868b
Mohammad-khk <mu92.sm11@gmail.com>
2021-06-03 13:42:25 +0200

    La till footer och ändrat bild

--- src/view/homeView/HomeView.tsx
+++ src/view/homeView/HomeView.tsx
@@ -1,19 +1,26 @@
-import './HomeView.css'
-import { useState, useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
-import RoutingPath from '../../routes/RoutingPath'
+import "./HomeView.css";
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import RoutingPath from "../../routes/RoutingPath";
 
 export const HomeView = () => {
-	const history = useHistory()
+  const history = useHistory();
 
-	return (
-		<div className="title">
-		  <h1>Sengångaren – Ett gulligt men långsamt djur</h1>
-		  <h2>1. Den tretåiga sengångaren simmar snabbare än den klättrar</h2>
-		  <img
-			src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV363mO-mTjpeXz_iqA2Uz9QwqPbTLGcT9fA&usqp=CAU"
-			alt="sengångare"
-		  ></img>
-		</div>
-	  );
-}
+  return (
+    <div>
+      <h1 className="title">Sengångaren – Ett gulligt men långsamt djur</h1>
+      {/* <h2>1. Den tretåiga sengångaren simmar snabbare än den klättrar</h2> */}
+
+      <img
+        className="img"
+        src="https://order.se/thumb/99231/620x0/972-1752_2.jpg"
+        alt="sengångare"
+      ></img>
+      <footer className="footer">
+        <p>
+          <a href="mailto:sengångaren.com">sengångaren@ däggdjur.com</a>
+        </p>
+      </footer>
+    </div>
+  );
+};
