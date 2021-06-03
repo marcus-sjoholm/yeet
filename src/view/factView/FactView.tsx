70547f27271f176b296bb9b457449960b6736efc
mathilda serneblad <mathilda.serneblad@icloud.com>
2021-06-03 13:37:44 +0200

    facts

--- src/view/factView/FactView.tsx
+++ src/view/factView/FactView.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import './FactView.css'
 
 export const FactView = () => {
     return (
         <div>
-            <h1 className="facts1">1. Sengångare är inte världens snabbaste simmare, 
-            men den tretåiga sengångaren simmar bra mycket snabbare i vatten än vad den rör sig på land.</h1>
-            <h1 className="facts2">2. Vilda sengångare sover omkring 9,5 – 10 timmar per dygn, vilket innebär att den sover längre än de flesta människor. 
-            Sengångare i fångenskap sover nästan dubbelt så länge som vilda sengångare.</h1>
-            <h1 className="facts3">3. Inom samma familj fanns tidigare en sorts sengångare som nästan var lika stora som 
-            vår tids elefanter. Dessa kallas för Amerikanska jättesengångare</h1>
-            <h1 className="facts4">4. Deras klor fungerar som hakar och kan bli uppemot 10 cm långa.</h1>
-            <h1 className="facts5">5. Däremot kan sengångarna kvickna till om de blir utsatta för fara. 
-            Då kan de slå till med klorna snabbare.</h1>
+            <br/>
+            <button className="facts1">Sengångare är inte världens snabbaste simmare, 
+            men den tretåiga sengångaren simmar bra mycket snabbare i vatten än vad den rör sig på land.</button> <br/>
+            <button className="facts2">Vilda sengångare sover omkring 9,5 – 10 timmar per dygn, vilket innebär att den sover längre än de flesta människor. 
+            Sengångare i fångenskap sover nästan dubbelt så länge som vilda sengångare.</button> <br/>
+            <button className="facts3">Inom samma familj fanns tidigare en sorts sengångare som nästan var lika stora som 
+            vår tids elefanter. Dessa kallas för Amerikanska jättesengångare</button> <br/>
+            <button className="facts4">Deras klor fungerar som hakar och kan bli uppemot 10 cm långa.</button> <br/>
+            <button className="facts5">Däremot kan sengångarna kvickna till om de blir utsatta för fara. 
+            Då kan de slå till med klorna snabbare.</button> <br/>
+            <button className="facts6">Livslängd: 2 år, men upp till 30 år i fångenskap.</button> <br/>
+            <button className="facts6"></button> <br/>
         </div>
     )
 }
