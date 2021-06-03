22a3532c4e83cb14e2e9681c640808a66cdf6dc9
Marcus Sjöholm <marcusasjoholm93@gmail.com>
2021-06-03 13:19:52 +0200

    Removed MobileView

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,12 +1,11 @@
 import { DeskopNavigation } from './desktopnavigation/DeskopNavigation'
-import { MobileNavigation } from './mobilenavigation/MobileNavigation'
 import { useWindowDimensions } from '../../hooks/useWindowDimensions'
 
 export const Navigation = () => {
 	const { width } = useWindowDimensions()
 
 	const displayNavigation = () => {
-		return width <= 1140 ? <MobileNavigation /> : <DeskopNavigation />
+		return <DeskopNavigation />
 	}
 
 	return (
