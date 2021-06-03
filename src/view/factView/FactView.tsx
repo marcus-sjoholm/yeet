ed07adc003ae3d406b5371c6751ba231bbb7018a
mathilda serneblad <mathilda.serneblad@icloud.com>
2021-06-03 13:20:59 +0200

    fact

--- src/view/factView/FactView.tsx
+++ src/view/factView/FactView.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 export const FactView = () => {
     return (
-        <div className="facts">
-            <h1>1. Sengångare är inte världens snabbaste simmare, 
+        <div>
+            <h1 className="facts1">1. Sengångare är inte världens snabbaste simmare, 
             men den tretåiga sengångaren simmar bra mycket snabbare i vatten än vad den rör sig på land.</h1>
-            <h1>2. Vilda sengångare sover omkring 9,5 – 10 timmar per dygn, vilket innebär att den sover längre än de flesta människor. 
+            <h1 className="facts2">2. Vilda sengångare sover omkring 9,5 – 10 timmar per dygn, vilket innebär att den sover längre än de flesta människor. 
             Sengångare i fångenskap sover nästan dubbelt så länge som vilda sengångare.</h1>
-            <h1>3. Inom samma familj fanns tidigare en sorts sengångare som nästan var lika stora som 
+            <h1 className="facts3">3. Inom samma familj fanns tidigare en sorts sengångare som nästan var lika stora som 
             vår tids elefanter. Dessa kallas för Amerikanska jättesengångare</h1>
-            <h1>4. Deras klor fungerar som hakar och kan bli uppemot 10 cm långa.</h1>
-            <h1>5. Däremot kan sengångarna kvickna till om de blir utsatta för fara. 
+            <h1 className="facts4">4. Deras klor fungerar som hakar och kan bli uppemot 10 cm långa.</h1>
+            <h1 className="facts5">5. Däremot kan sengångarna kvickna till om de blir utsatta för fara. 
             Då kan de slå till med klorna snabbare.</h1>
         </div>
     )
