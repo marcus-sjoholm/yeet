de9edbc0108b768c6ccfec89127bfb8ff1793018
Marcus Sjöholm <marcusasjoholm93@gmail.com>
2021-06-03 13:38:33 +0200

    bilder

--- src/components/navigation/desktopnavigation/DeskopNavigation.tsx
+++ src/components/navigation/desktopnavigation/DeskopNavigation.tsx
@@ -3,6 +3,7 @@ import { useHistory } from 'react-router-dom'
 import RoutingPath from '../../../routes/RoutingPath'
 import HomeButton from '../../../assets/images/home.png'
 import GalleryButton from '../../../assets/images/gallery.png'
+import InfoButton from '../../../assets/images/info.png'
 
 
 export const DeskopNavigation = () => {
@@ -13,7 +14,7 @@ export const DeskopNavigation = () => {
 		<div className='desktopNavigationWrapper'>
 			<img src={HomeButton} className='home' onClick={() => history.push(RoutingPath.homeView)} />
 			<img src={GalleryButton} className='gallery' onClick={() => history.push(RoutingPath.galleryView)} />
-			<span className='fact' onClick={() => history.push(RoutingPath.factView)}>Fakta</span>
+			<img src={InfoButton} className='info' onClick={() => history.push(RoutingPath.factView)} />
 		</div>
 	)
 }
